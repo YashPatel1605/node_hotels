57d3f4934aaeb1ce8c0deec061322b0e13a670ae
Yash <patelyash9712@gmail.com>
2024-06-13 15:32:00 +0530

    add some new feature like authentication data etc..

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const db = require("./db");
 const app = express();
+const personRouter = require("./routes/personRoutes");
+const dataRoutes = require("./routes/dataRoutes");
+const passport = require("./auth");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
 
-const personRouter = require("./routes/personRoutes");
-const dataRoutes = require("./routes/dataRoutes");
+//middleware function
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request made to :${req.originalUrl}`
+  );
+  next(); //move to the next phase
+};
+
+app.use(logRequest);
+app.use(passport.initialize());
+
+const localAuthMiddelware = passport.authenticate("local", { session: false });
+app.get("/", (req, res) => {
+  res.send("welcome to the hotels");
+});
 
 app.use("/person", personRouter);
 app.use("/data", dataRoutes);
